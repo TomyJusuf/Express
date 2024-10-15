7cde4ef0a2b6b7d7bdddb52aed237c2b71e1b414
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-15 17:38:32 +0200

    Starting with basic of middleware

--- express201/appUse.js
+++ express201/appUse.js
@@ -16,25 +16,25 @@ function validateUser(req, res, next) {
 }
 
 // This will run validate on ALL paths,all methods
-app.use('/admin', validateUser)
-
-// This will run validate only on /
-app.get('/', validateUser)
+app.use('/', validateUser)
 
 // This will run validate only on /admin
-app.use('/admin', (res, req, next) => {
+app.use('/admin', (req, res, next) => {
   res.locals.validated = true
-  console.log('User is validated')
+  console.log('User is validated /admin')
   next()
 })
 
+// // This will run validate only on /
+app.get('/', validateUser)
+
 app.get('/', (req, res, next) => {
-  res.send('<h1> Hello from Express</h1>')
+  res.send('<h1> Main page</h1>')
   console.log(res.locals.validated)
 })
 
 app.get('/admin', (req, res, next) => {
-  res.send('<h1> Hello from Express</h1>')
+  res.send('<h1> Admin page</h1>')
   console.log(res.locals.validated)
 })
 

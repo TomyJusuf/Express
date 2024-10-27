ad44df57160668f98acd29caba3de43413b46f4c
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-27 19:12:41 +0100

    Add routing to app.js and also include helmet middleware

--- buildingAPI/app.js
+++ buildingAPI/app.js
@@ -4,6 +4,18 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 const helmet = require('helmet')
+var app = express()
+
+app.use(helmet())
+
+app.use((req, res, next) => {
+  if (req.query.api_key !== '12345') {
+    res.status(401) // unauthorized == 401, NOT 201
+    res.json('Unauthorized')
+  } else {
+    next()
+  }
+})
 
 // now_playing
 var indexRouter = require('./routes/index')
@@ -11,9 +23,6 @@ var indexRouter = require('./routes/index')
 const movieRouter = require('./routes/movies')
 //  /search/...
 const searchRouter = require('./routes/search')
-
-var app = express()
-app.use(helmet())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -26,7 +35,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/movies', movieRouter)
+app.use('/movie', movieRouter)
+
 app.use('/search', searchRouter)
 
 // catch 404 and forward to error handler
